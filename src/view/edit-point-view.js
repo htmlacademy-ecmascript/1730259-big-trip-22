2b32ba43a5f426d6c210068398f27cd09da01227
Kvazim <mesvladimir@gmail.com>
2023-12-14 20:41:00 +0300

    feat: Добавление городов destination fix: Правка Чекбокса типов

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -2,10 +2,12 @@ import { DateFormat, POINTS_TYPE } from '../const.js';
 import {createElement} from '../render.js';
 import { capitalize, getElementById, getElementByType, humanizeTaskDueDate } from '../utils.js';
 
-function createTypeTemplate(type) {
+function createTypeTemplate(type, checkedType) {
+  const isChecked = checkedType === type ? 'checked' : false;
+
   return (
     `<div class="event__type-item">
-      <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}">
+      <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}" ${isChecked}>
       <label class="event__type-label  event__type-label--${type}" for="event-type-${type}-1">${capitalize(type)}</label>
     </div>`
   );
@@ -13,7 +15,7 @@ function createTypeTemplate(type) {
 
 function createOfferTemplate(offer, checkedOffers) {
   const {id, title, price} = offer;
-  const isChecked = checkedOffers.includes(id) ? 'checked' : '';
+  const isChecked = checkedOffers.includes(id) ? 'checked' : false;
 
   return (
     `<div class="event__offer-selector">
@@ -76,7 +78,7 @@ function createDestinationTemplate(destination) {
 }
 
 function createEditPointTemplate(point, offers, destination) {
-  const { type, dateFrom, dateTo, basePrice, offers: checkedOffers, destination: pointDestination } = point;
+  const { id, type, dateFrom, dateTo, basePrice, offers: checkedOffers, destination: pointDestination } = point;
   const filteredOfferByType = getElementByType(offers, type);
   const filteredDestinationById = getElementById(destination, pointDestination);
   const { name } = filteredDestinationById;
@@ -87,47 +89,45 @@ function createEditPointTemplate(point, offers, destination) {
         <form class="event event--edit" action="#" method="post">
           <header class="event__header">
             <div class="event__type-wrapper">
-              <label class="event__type  event__type-btn" for="event-type-toggle-1">
+              <label class="event__type  event__type-btn" for="event-type-toggle-${id}">
                 <span class="visually-hidden">Choose event type</span>
                 <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
               </label>
-              <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
+              <input class="event__type-toggle  visually-hidden" id="event-type-toggle-${id}" type="checkbox">
 
               <div class="event__type-list">
                 <fieldset class="event__type-group">
                   <legend class="visually-hidden">Event type</legend>
 
-                  ${POINTS_TYPE.map((item) => createTypeTemplate(item)).join('')}
+                  ${POINTS_TYPE.map((item) => createTypeTemplate(item, type)).join('')}
                 </fieldset>
               </div>
             </div>
 
             <div class="event__field-group  event__field-group--destination">
-              <label class="event__label  event__type-output" for="event-destination-1">
+              <label class="event__label  event__type-output" for="event-destination-${id}">
                 ${type}
               </label>
-              <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value='${name}' list="destination-list-1">
-              <datalist id="destination-list-1">
-                <option value="Amsterdam"></option>
-                <option value="Geneva"></option>
-                <option value="Chamonix"></option>
+              <input class="event__input  event__input--destination" id="event-destination-${id}" type="text" name="event-destination" value='${name}' list="destination-list-${id}">
+              <datalist id="destination-list-${id}">
+                ${destination.map((item) => `<option value=${item.name}></option>`)}
               </datalist>
             </div>
 
             <div class="event__field-group  event__field-group--time">
-              <label class="visually-hidden" for="event-start-time-1">From</label>
-              <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value=${humanizeTaskDueDate(dateFrom, DateFormat.DAY_MONTH_YEAR)}>
+              <label class="visually-hidden" for="event-start-time-${id}">From</label>
+              <input class="event__input  event__input--time" id="event-start-time-${id}" type="text" name="event-start-time" value=${humanizeTaskDueDate(dateFrom, DateFormat.DAY_MONTH_YEAR)}>
               &mdash;
-              <label class="visually-hidden" for="event-end-time-1">To</label>
-              <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value=${humanizeTaskDueDate(dateTo, DateFormat.DAY_MONTH_YEAR)}>
+              <label class="visually-hidden" for="event-end-time-${id}">To</label>
+              <input class="event__input  event__input--time" id="event-end-time-${id}" type="text" name="event-end-time" value=${humanizeTaskDueDate(dateTo, DateFormat.DAY_MONTH_YEAR)}>
             </div>
 
             <div class="event__field-group  event__field-group--price">
-              <label class="event__label" for="event-price-1">
+              <label class="event__label" for="event-price-${id}">
                 <span class="visually-hidden">Price</span>
                 &euro;
               </label>
-              <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value=${basePrice}>
+              <input class="event__input  event__input--price" id="event-price-${id}" type="text" name="event-price" value=${basePrice}>
             </div>
 
             <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
