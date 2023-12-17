2182b8ca512e693832f0c71ce81dfade1e1afeaa
Kvazim <mesvladimir@gmail.com>
2023-12-17 15:09:51 +0300

    feat: Добавил презентерам приватные поля

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -5,37 +5,46 @@ import WaypointView from '../view/waypoint-view';
 import SortListView from '../view/sort-list-view';
 
 export default class BoardPresenter {
-  weapointListView = new WeapointListView();
-  sortListView = new SortListView();
+  #boardContainer = null;
+  #pointModel = null;
 
+  #weapointListView = new WeapointListView();
+  #sortListView = new SortListView();
+
+  #boardPoint = [];
 
   constructor({boardContainer, pointModel}) {
-    this.boardContainer = boardContainer;
-    this.pointModel = pointModel;
+    this.#boardContainer = boardContainer;
+    this.#pointModel = pointModel;
+  }
+
+  #renderPoint(data) {
+    const pointComponent = new WaypointView(data);
+    render(pointComponent, this.#weapointListView.element);
   }
 
   init() {
-    const points = this.pointModel.getPoints();
-    const offers = this.pointModel.getOffers();
-    const destinations = this.pointModel.getDestinations();
+    const points = this.#pointModel.points;
+    const offers = this.#pointModel.offers;
+    const destinations = this.#pointModel.destinations;
 
-    this.boardPoint = [...points];
-    render(this.sortListView, this.boardContainer);
-    render(this.weapointListView, this.boardContainer);
+    this.#boardPoint = [...points];
+
+    render(this.#sortListView, this.#boardContainer);
+    render(this.#weapointListView, this.#boardContainer);
 
     render(new EditPointView({
-      points: this.boardPoint[0],
+      points: this.#boardPoint[0],
       offers: offers,
       destinations: destinations
-    }), this.weapointListView.element);
+    }), this.#weapointListView.element);
 
-    for (let i = 1; i < this.boardPoint.length; i++) {
-      render(new WaypointView({
-        points: this.boardPoint[i],
+    for (let i = 1; i < this.#boardPoint.length; i++) {
+      this.#renderPoint({
+        points: this.#boardPoint[i],
         offers: offers,
         destinations: destinations
-      }), this.weapointListView.element);
+      });
     }
-
   }
 }
--- src/presenter/filter-presenter.js
+++ src/presenter/filter-presenter.js
@@ -2,14 +2,15 @@ import { render } from '../framework/render.js';
 import FilterListView from '../view/filter-list-view.js';
 
 export default class FilterPresenter {
-  filterListView = new FilterListView();
+  #filterConteiner = null;
 
+  #filterListView = new FilterListView();
 
   constructor({filterConteiner}) {
-    this.filterConteiner = filterConteiner;
+    this.#filterConteiner = filterConteiner;
   }
 
   init() {
-    render(this.filterListView, this.filterConteiner);
+    render(this.#filterListView, this.#filterConteiner);
   }
 }
