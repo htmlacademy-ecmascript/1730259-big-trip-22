ae8059e44a803cb25ff87d730a69fc5794d96ed2
Kvazim <mesvladimir@gmail.com>
2023-12-18 20:42:26 +0300

    feat: добавил в ситемное вью отоброжение через параметр

--- src/view/system-message-viev.js
+++ src/view/system-message-viev.js
@@ -1,11 +1,19 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
-function createSystemMessageTemplate() {
-  return '<p class="trip-events__msg">Loading...</p>';
+function createSystemMessageTemplate(message) {
+  return `<p class="trip-events__msg">${message}</p>`;
 }
 
 export default class SystemMessageView extends AbstractView {
+  #message = null;
+
+  constructor({message}) {
+    super();
+
+    this.#message = message;
+  }
+
   get template() {
-    return createSystemMessageTemplate();
+    return createSystemMessageTemplate(this.#message);
   }
 }
